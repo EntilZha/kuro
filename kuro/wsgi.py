2ee6f604d8df92fd336f645a07c6e9876b6363c8
EntilZha <ski.rodriguez@gmail.com>
2018-01-29 19:26:34 -0700

    Hack to serve static files

--- kuro/wsgi.py
+++ kuro/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kuro.settings")
 
-application = get_wsgi_application()
+# StaticFilesHandler here is a hack, should fix at some point
+application = StaticFilesHandler(get_wsgi_application())
